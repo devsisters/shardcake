bd20d6d19befb5f388122c5462c8100cc6fb0359
WonRyool Kim <85146842+wonryool-kim@users.noreply.github.com>
2022-09-27 10:47:31 +0900

    Add log when server start and abnormally stop(series/2.x) (#20)
    * Add log
    
    * Update manager/src/main/scala/com/devsisters/shardcake/Server.scala
    
    Co-authored-by: Pierre Ricadat <ghostdogpr@users.noreply.github.com>
    
    * Apply feedback
    
    * fmt
    
    Co-authored-by: Pierre Ricadat <ghostdogpr@users.noreply.github.com>

--- manager/src/main/scala/com/devsisters/shardcake/Server.scala
+++ manager/src/main/scala/com/devsisters/shardcake/Server.scala
@@ -2,9 +2,10 @@ package com.devsisters.shardcake
 
 import caliban.ZHttpAdapter
 import caliban.wrappers.Wrappers.printErrors
-import zhttp.http.Middleware.cors
 import zhttp.http._
-import zhttp.service.{ Server => ZServer }
+import zhttp.http.Middleware.cors
+import zhttp.service.{ EventLoopGroup, Server => ZServer }
+import zhttp.service.server.ServerChannelFactory
 import zio._
 
 object Server {
@@ -16,14 +17,19 @@ object Server {
     for {
       config      <- ZIO.service[ManagerConfig]
       interpreter <- (GraphQLApi.api @@ printErrors).interpreter
-      nothing     <- ZServer
-                       .start(
-                         config.apiPort,
-                         Http.collectHttp[Request] {
-                           case _ -> !! / "health"          => Http.succeed(Response.ok)
-                           case _ -> !! / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
-                           case _ -> !! / "ws" / "graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
-                         } @@ cors()
+      server       = ZServer.port(config.apiPort) ++ ZServer.app(Http.collectHttp[Request] {
+                       case _ -> !! / "health"          => Http.succeed(Response.ok)
+                       case _ -> !! / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
+                       case _ -> !! / "ws" / "graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
+                     } @@ cors())
+      nothing     <- ZIO
+                       .scoped(
+                         server.make
+                           .flatMap(start =>
+                             ZIO.logInfo(s"Shard Manager server started on port ${start.port}.") *>
+                               ZIO.never
+                           )
+                           .provideSomeLayer[ShardManager with Scope](EventLoopGroup.auto(0) ++ ServerChannelFactory.auto)
                        )
                        .forever
     } yield nothing
