33eb5a098994cdcd9408402ae2cdfcceea827687
Wonryool Kim <wonryool.kim@devsisters.com>
2022-09-27 10:35:15 +0900

    Apply feedback

--- examples/src/main/scala/example/complex/ShardManagerApp.scala
+++ examples/src/main/scala/example/complex/ShardManagerApp.scala
@@ -5,20 +5,15 @@ import com.devsisters.shardcake.interfaces._
 import zio._
 
 object ShardManagerApp extends ZIOAppDefault {
-  def run: Task[Any] =
-    Server.run
-      .provide(
-        ZLayer.succeed(ManagerConfig.default),
-        ZLayer.succeed(GrpcConfig.default),
-        ZLayer.succeed(RedisConfig.default),
-        redis,
-        StorageRedis.live, // store data in Redis
-        PodsHealth.local,  // just ping a pod to see if it's alive
-        GrpcPods.live,     // use gRPC protocol
-        ShardManager.live  // Shard Manager logic
-      )
-      .catchAllCause(cause =>
-        ZIO
-          .when(!cause.isInterrupted)(ZIO.logErrorCause(s"Shard cake failed, because of ${cause.prettyPrint}", cause))
-      )
+  def run: Task[Nothing] =
+    Server.run.provide(
+      ZLayer.succeed(ManagerConfig.default),
+      ZLayer.succeed(GrpcConfig.default),
+      ZLayer.succeed(RedisConfig.default),
+      redis,
+      StorageRedis.live, // store data in Redis
+      PodsHealth.local,  // just ping a pod to see if it's alive
+      GrpcPods.live,     // use gRPC protocol
+      ShardManager.live  // Shard Manager logic
+    )
 }
