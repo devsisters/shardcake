1d062c970a1e0f82323ac70a0f532fd87481cb25
WonRyool Kim <85146842+wonryool-kim@users.noreply.github.com>
2022-09-27 10:43:44 +0900

    Add log when server start and abnormally stop(series/1.x) (#21)
    * Add log
    
    * Apply feedback

--- manager/src/main/scala/com/devsisters/shardcake/Server.scala
+++ manager/src/main/scala/com/devsisters/shardcake/Server.scala
@@ -2,9 +2,11 @@ package com.devsisters.shardcake
 
 import caliban.ZHttpAdapter
 import caliban.wrappers.Wrappers.printErrors
+import com.devsisters.shardcake.interfaces.Logging
 import zhttp.http.Middleware.cors
 import zhttp.http._
-import zhttp.service.{ Server => ZServer }
+import zhttp.service.{ EventLoopGroup, Server => ZServer }
+import zhttp.service.server.ServerChannelFactory
 import zio._
 import zio.clock.Clock
 import zio.console.Console
@@ -14,18 +16,23 @@ object Server {
   /**
    * Start an HTTP server that exposes the Shard Manager GraphQL API
    */
-  val run: RIO[Has[ShardManager] with Has[ManagerConfig] with Clock with Console, Nothing] =
+  val run: RIO[Has[ShardManager] with Has[ManagerConfig] with Has[Logging] with Clock with Console, Nothing] =
     for {
       config      <- ZIO.service[ManagerConfig]
+      logger      <- ZIO.service[Logging]
       interpreter <- (GraphQLApi.api @@ printErrors).interpreter
-      nothing     <- ZServer
-                       .start(
-                         config.apiPort,
-                         Http.collectHttp[Request] {
-                           case _ -> !! / "health"          => Http.succeed(Response.ok)
-                           case _ -> !! / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
-                           case _ -> !! / "ws" / "graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
-                         } @@ cors()
+      server       = ZServer.port(config.apiPort) ++ ZServer.app(Http.collectHttp[Request] {
+                       case _ -> !! / "health"          => Http.succeed(Response.ok)
+                       case _ -> !! / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
+                       case _ -> !! / "ws" / "graphql"  => ZHttpAdapter.makeWebSocketService(interpreter)
+                     } @@ cors())
+      nothing     <- server.make
+                       .use(start =>
+                         logger.logInfo(s"Server Manager started on port ${start.port}")
+                           *> ZIO.never
+                       )
+                       .provideSomeLayer[Console with Has[ShardManager] with Clock](
+                         ServerChannelFactory.auto ++ EventLoopGroup.auto(0)
                        )
                        .forever
     } yield nothing
