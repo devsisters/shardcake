e9225ce488c334b6d18c2072df41a6bf34c42701
Pierre Ricadat <pierre.ricadat@devsisters.com>
2022-10-18 10:00:40 +0900

    Polishing

--- entities/src/main/scala/com/devsisters/shardcake/Sharding.scala
+++ entities/src/main/scala/com/devsisters/shardcake/Sharding.scala
@@ -420,6 +420,12 @@ object Sharding {
   ): URIO[Sharding with Scope with R, Unit] =
     ZIO.serviceWithZIO[Sharding](_.registerEntity[R, Req](entityType, behavior, terminateMessage))
 
+  /**
+   * Register a new topic, allowing pods to broadcast messages to subscribers.
+   * It takes a `behavior` which is a function from a topic and a queue of messages to a ZIO computation that runs forever and consumes those messages.
+   * You can use `ZIO.interrupt` from the behavior to stop it (it will be restarted the next time the topic receives a message).
+   * If provided, the optional `terminateMessage` will be sent to the topic before it is stopped, allowing for cleanup logic.
+   */
   def registerTopic[R, Req: Tag](
     topicType: Topic[Req],
     behavior: (String, Dequeue[Req]) => RIO[R, Nothing],
