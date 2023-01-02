95e1c48439ff02e2463f9c13edb19b52aa7e1b04
Pierre Ricadat <ghostdogpr@gmail.com>
2023-01-02 23:05:43 +0900

    Improve

--- entities/src/main/scala/com/devsisters/shardcake/internal/ReplyChannel.scala
+++ entities/src/main/scala/com/devsisters/shardcake/internal/ReplyChannel.scala
@@ -7,30 +7,29 @@ private[shardcake] sealed trait ReplyChannel[-A] { self =>
   val await: UIO[Unit]
   val end: UIO[Unit]
   def fail(cause: Cause[Throwable]): UIO[Unit]
-  protected def reply(a: A): UIO[Unit]
-
-  def replySingle(a: A): UIO[Unit] =
-    reply(a) *> end
-
-  def replyStream(stream: ZStream[Any, Throwable, A]): UIO[Unit] =
-    (stream.runForeach(reply) *> end).catchAllCause(fail).fork.unit
+  def replySingle(a: A): UIO[Unit]
+  def replyStream(stream: ZStream[Any, Throwable, A]): UIO[Unit]
 }
 
 private[shardcake] object ReplyChannel {
   case class FromQueue[A](queue: Queue[Take[Throwable, A]]) extends ReplyChannel[A] {
-    val await: UIO[Unit]                         = queue.awaitShutdown
-    val end: UIO[Unit]                           = queue.offer(Take.end).unit
-    def fail(cause: Cause[Throwable]): UIO[Unit] = queue.offer(Take.failCause(cause)).unit
-    def reply(a: A): UIO[Unit]                   = queue.offer(Take.single(a)).unit
-    val output: ZStream[Any, Throwable, A]       = ZStream.fromQueue(queue).flattenTake
+    val await: UIO[Unit]                                           = queue.awaitShutdown
+    val end: UIO[Unit]                                             = queue.offer(Take.end).unit
+    def fail(cause: Cause[Throwable]): UIO[Unit]                   = queue.offer(Take.failCause(cause)).unit
+    def replySingle(a: A): UIO[Unit]                               = queue.offer(Take.single(a)) *> end
+    def replyStream(stream: ZStream[Any, Throwable, A]): UIO[Unit] =
+      (stream.runForeach(a => queue.offer(Take.single(a))) *> end).catchAllCause(fail).fork.unit
+    val output: ZStream[Any, Throwable, A]                         = ZStream.fromQueue(queue).flattenTake
   }
 
   case class FromPromise[A](promise: Promise[Throwable, Option[A]]) extends ReplyChannel[A] {
-    val await: UIO[Unit]                         = promise.await.exit.unit
-    val end: UIO[Unit]                           = promise.succeed(None).unit
-    def fail(cause: Cause[Throwable]): UIO[Unit] = promise.failCause(cause).unit
-    def reply(a: A): UIO[Unit]                   = promise.succeed(Some(a)).unit
-    val output: Task[Option[A]]                  = promise.await
+    val await: UIO[Unit]                                           = promise.await.exit.unit
+    val end: UIO[Unit]                                             = promise.succeed(None).unit
+    def fail(cause: Cause[Throwable]): UIO[Unit]                   = promise.failCause(cause).unit
+    def replySingle(a: A): UIO[Unit]                               = promise.succeed(Some(a)).unit
+    def replyStream(stream: ZStream[Any, Throwable, A]): UIO[Unit] =
+      stream.runHead.flatMap(promise.succeed(_)).catchAllCause(fail).fork.unit
+    val output: Task[Option[A]]                                    = promise.await
   }
 
   def single[A]: UIO[FromPromise[A]] =
