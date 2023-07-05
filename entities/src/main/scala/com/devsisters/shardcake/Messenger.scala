4ce92c022bd97b3b7d46523c13891f4be9aeee18
Matthieu Leclercq <matthieu@automat.ai>
2023-07-05 18:53:54 -0400

    Add sendStreamAutoRestart

--- entities/src/main/scala/com/devsisters/shardcake/Messenger.scala
+++ entities/src/main/scala/com/devsisters/shardcake/Messenger.scala
@@ -1,6 +1,7 @@
 package com.devsisters.shardcake
 
-import zio.Task
+import com.devsisters.shardcake.errors.PodUnavailable
+import zio._
 import zio.stream.ZStream
 
 /**
@@ -23,4 +24,31 @@ trait Messenger[-Msg] {
    * Send a message and receive a stream of responses of type `Res`
    */
   def sendStream[Res](entityId: String)(msg: Replier[Res] => Msg): Task[ZStream[Any, Throwable, Res]]
+
+  /**
+   * Send a message and receive a stream of responses of type `Res` and restart the stream when the remote entity is
+   * rebalanced.
+   *
+   * To do so, we need a "cursor" so the stream of responses can be restarted where it ended before the rebalance. That
+   * is, the first message sent to the remote entity contains the given initial cursor value and we extract an updated
+   * cursor from the responses so that when the remote entity is rebalanced, a new message can be sent with the last
+   * cursor we've seen in the previous stream of responses.
+   */
+  def sendStreamAutoRestart[Cursor, Res](entityId: String, cursor: Cursor)(msg: (Cursor, Replier[Res]) => Msg)(
+    updateCursor: (Cursor, Res) => Cursor
+  ): ZStream[Any, Throwable, Res] =
+    ZStream
+      .unwrap(sendStream(entityId)(msg(cursor, _)))
+      .either
+      .mapAccum(cursor) {
+        case (c, Right(res)) => updateCursor(c, res) -> Right(res)
+        case (c, Left(err))  => (c, Left(c -> err))
+      }
+      .flatMap {
+        case Right(res)                                => ZStream.succeed(res)
+        case Left((lastSeenCursor, PodUnavailable(_))) =>
+          ZStream.execute(ZIO.sleep(200.millis)) ++
+            sendStreamAutoRestart(entityId, lastSeenCursor)(msg)(updateCursor)
+        case Left((_, err))                            => ZStream.fail(err)
+      }
 }
