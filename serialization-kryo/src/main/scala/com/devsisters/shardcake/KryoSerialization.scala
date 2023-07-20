ef803c88ec603d8bf923da6999a625a9c00d5f27
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2023-07-20 15:11:56 +0900

    Switch Kryo serialization library (1.x) (#76)
    * Switch Kryo serialization library
    
    * Remove crossversion
    
    * Use latest version of scala kryo

--- serialization-kryo/src/main/scala/com/devsisters/shardcake/KryoSerialization.scala
+++ serialization-kryo/src/main/scala/com/devsisters/shardcake/KryoSerialization.scala
@@ -1,23 +1,32 @@
 package com.devsisters.shardcake
 
 import com.devsisters.shardcake.interfaces.Serialization
-import com.twitter.chill.{ KryoInstantiator, KryoPool, ScalaKryoInstantiator }
+import com.typesafe.config.{ Config, ConfigFactory }
+import io.altoo.serialization.kryo.scala.ScalaKryoSerializer
 import zio.{ Has, Task, ZIO, ZLayer }
 
 object KryoSerialization {
 
   /**
-   * A layer that returns a serialization implementation using the Kryo library
+   * A layer that returns a serialization implementation using the Kryo library.
    */
   val live: ZLayer[Any, Throwable, Has[Serialization]] =
+    ZIO.effect(ConfigFactory.defaultReference()).flatMap(make).toLayer
+
+  /**
+   * A layer that returns a serialization implementation using the Kryo library, taking a Config object.
+   * See https://github.com/altoo-ag/scala-kryo-serialization for more details about configuration.
+   */
+  def liveWithConfig(config: Config): ZLayer[Any, Throwable, Has[Serialization]] =
+    make(config).toLayer
+
+  private def make(config: Config): Task[Serialization] =
     ZIO.effect {
-      def kryoInstantiator: KryoInstantiator = new ScalaKryoInstantiator
-      def poolSize: Int                      = 4 * java.lang.Runtime.getRuntime.availableProcessors
-      KryoPool.withByteArrayOutputStream(poolSize, kryoInstantiator)
-    }.map(kryoPool =>
+      new ScalaKryoSerializer(config, getClass.getClassLoader)
+    }.map(serializer =>
       new Serialization {
-        def encode(message: Any): Task[Array[Byte]] = ZIO.effect(kryoPool.toBytesWithClass(message))
-        def decode[A](bytes: Array[Byte]): Task[A]  = ZIO.effect(kryoPool.fromBytes(bytes).asInstanceOf[A])
+        def encode(message: Any): Task[Array[Byte]] = ZIO.fromTry(serializer.serialize(message))
+        def decode[A](bytes: Array[Byte]): Task[A]  = ZIO.fromTry(serializer.deserialize[A](bytes))
       }
-    ).toLayer
+    )
 }
