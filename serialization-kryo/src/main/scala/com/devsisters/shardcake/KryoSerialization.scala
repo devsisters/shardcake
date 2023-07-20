ea0a466571bcd4bfdd7eb9dd3abb262a9fc4338e
Pierre Ricadat <ghostdogpr@users.noreply.github.com>
2023-07-20 15:12:00 +0900

    Switch Kryo serialization library (#82)
    # Conflicts:
    #	serialization-kryo/src/main/scala/com/devsisters/shardcake/KryoSerialization.scala

--- serialization-kryo/src/main/scala/com/devsisters/shardcake/KryoSerialization.scala
+++ serialization-kryo/src/main/scala/com/devsisters/shardcake/KryoSerialization.scala
@@ -1,25 +1,32 @@
 package com.devsisters.shardcake
 
 import com.devsisters.shardcake.interfaces.Serialization
-import com.twitter.chill.{ KryoInstantiator, KryoPool, ScalaKryoInstantiator }
+import com.typesafe.config.{ Config, ConfigFactory }
+import io.altoo.serialization.kryo.scala.ScalaKryoSerializer
 import zio.{ Task, ZIO, ZLayer }
 
 object KryoSerialization {
 
   /**
-   * A layer that returns a serialization implementation using the Kryo library
+   * A layer that returns a serialization implementation using the Kryo library.
    */
   val live: ZLayer[Any, Throwable, Serialization] =
-    ZLayer {
-      ZIO.attempt {
-        def kryoInstantiator: KryoInstantiator = new ScalaKryoInstantiator
-        def poolSize: Int                      = 4 * java.lang.Runtime.getRuntime.availableProcessors
-        KryoPool.withByteArrayOutputStream(poolSize, kryoInstantiator)
-      }.map(kryoPool =>
-        new Serialization {
-          def encode(message: Any): Task[Array[Byte]] = ZIO.attempt(kryoPool.toBytesWithClass(message))
-          def decode[A](bytes: Array[Byte]): Task[A]  = ZIO.attempt(kryoPool.fromBytes(bytes).asInstanceOf[A])
-        }
-      )
-    }
+    ZLayer(ZIO.attempt(ConfigFactory.defaultReference()).flatMap(make))
+
+  /**
+   * A layer that returns a serialization implementation using the Kryo library, taking a Config object.
+   * See https://github.com/altoo-ag/scala-kryo-serialization for more details about configuration.
+   */
+  def liveWithConfig(config: Config): ZLayer[Any, Throwable, Serialization] =
+    ZLayer(make(config))
+
+  private def make(config: Config): Task[Serialization] =
+    ZIO.attempt {
+      new ScalaKryoSerializer(config, getClass.getClassLoader)
+    }.map(serializer =>
+      new Serialization {
+        def encode(message: Any): Task[Array[Byte]] = ZIO.fromTry(serializer.serialize(message))
+        def decode[A](bytes: Array[Byte]): Task[A]  = ZIO.fromTry(serializer.deserialize[A](bytes))
+      }
+    )
 }
