96abe18f0bd8a6253f23d320d8825ff5cfb9070b
Pierre Ricadat <pierre.ricadat@devsisters.com>
2024-03-19 17:40:19 +0900

    Make grpc executor configurable

--- protocol-grpc/src/main/scala/com/devsisters/shardcake/GrpcConfig.scala
+++ protocol-grpc/src/main/scala/com/devsisters/shardcake/GrpcConfig.scala
@@ -1,12 +1,16 @@
 package com.devsisters.shardcake
 
+import java.util.concurrent.Executor
+
 /**
  * The configuration for the gRPC client.
+ *
  * @param maxInboundMessageSize the maximum message size allowed to be received by the grpc client
+ * @param executor a custom executor to pass to grpc-java when creating gRPC clients and servers
  */
-case class GrpcConfig(maxInboundMessageSize: Int)
+case class GrpcConfig(maxInboundMessageSize: Int, executor: Option[Executor])
 
 object GrpcConfig {
   val default: GrpcConfig =
-    GrpcConfig(maxInboundMessageSize = 32 * 1024 * 1024)
+    GrpcConfig(maxInboundMessageSize = 32 * 1024 * 1024, None)
 }
